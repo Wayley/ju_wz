486c774ae72cb658bc2ef846cae9cf8d3cb248f4
wzheng <hb_wangzheng@163.com>
2019-04-28 16:51:10 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ju_wx'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
