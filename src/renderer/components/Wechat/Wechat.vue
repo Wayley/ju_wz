ea1155332c157c57bcd1d2fa781c76193acef3a6
wzheng <hb_wangzheng@163.com>
2019-05-09 15:47:02 +0800

    modified Wechat.vue

--- src/renderer/components/Wechat/Wechat.vue
+++ src/renderer/components/Wechat/Wechat.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="wechat-page">
-    <webview ref="foo" class="wechat-view" id="foo" autosize="on" src="https://wx.qq.com/" nodeintegration></webview>
+    <div class="indicator"></div>
+    <webview id="foo" src="https://wx.qq.com/" style="display:inline-flex; width:900px; height:500px" nodeintegration allowpopups></webview>
   </div>
 </template>
 <script>
@@ -9,41 +10,96 @@ export default {
     return {}
   },
   created () {
-    // const BrowserWindow = require('electron').remote.BrowserWindow
-    // let win = new BrowserWindow({
-    //   width: 1100,
-    //   height: 716,
-    //   frame: true,
-    //   autoHideMenuBar: false,
-    //   maximizable: false,
-    //   resizable: false,
-    //   show: false
-    // })
-    // win.loadURL('http://github.com')
-    // let contents = win.webContents
-    // win.webContents.on('did-navigate', (event, url) => {
-    //   // 对应操作
-    //   console.log(event, url, '====')
-    // })
-    // console.log(contents, './//////////////')
+    onload = () => {
+      const webview = document.querySelector('webview')
+      const indicator = document.querySelector('.indicator')
+      const loadstart = () => {
+        indicator.innerText = 'loading...'
+      }
+      const loadstop = () => {
+        indicator.innerText = 'loaded~~~~~'
+      }
+      webview.addEventListener('did-start-loading', loadstart)
+      webview.addEventListener('did-stop-loading', loadstop)
+
+      webview.addEventListener('new-window', e => {
+        console.log('new-window', e)
+      })
+    }
   },
   mounted () {
-    var webview = this.$refs['foo']
-    webview.addEventListener('dom-ready', function () {
-      // 打开webview 的调试窗口
-      // webview.openDevTools()
-      console.log(webview.getURL())
+    const webview = document.querySelector('webview')
+    webview.addEventListener('dom-ready', () => {
+      let result = webview.querySelector('div')
+      console.log('dom-ready', result)
+
+      this.insertCSS(webview)
+      this.executeJavaScript(webview)
+      // this.test(webview)
     })
-    console.log(webview, '------')
+    webview.addEventListener('did-finish-load', () => {})
   },
   methods: {
     onload () {
       console.log(11111)
+    },
+    insertCSS (webview) {
+      webview && webview.insertCSS(`body, p{color: red !important;}`)
+    },
+    executeJavaScript (webview) {
+      webview &&
+        webview.executeJavaScript(
+          `document.documentElement.innerHTML`,
+          // `document.querySelector('webview').documentElement.innerHTML`,
+          function (str) {
+            console.log(str)
+
+            let doc = document.createDocumentFragment()
+            let el = document.createElement('html')
+            el.innerHTML = str
+            doc.appendChild(el)
+            // ////////////////////
+            setTimeout(function () {
+              var userInfoEl = doc.querySelector('.header')
+              var imgEl = doc.querySelector('.header .img')
+              var nickNameEl = doc.querySelector('.header .info .nickname span')
+              var avatar = imgEl.src
+              var nickNameStr = nickNameEl.innerHTML
+
+              var appElement = doc.querySelector(
+                '[ng-controller=appController]'
+              )
+              const angular = window.angular
+
+              // 获取$scope变量
+              // var appElement =angular.element(document.findElementById('elementId');
+
+              var $scope = angular.element(appElement).scope()
+              console.log(appElement, '------------', $scope)
+
+              console.log(userInfoEl, 1234567890987654321, avatar, nickNameStr)
+              console.log('avatar:', avatar)
+              console.log('nickName:', nickNameStr)
+            }, 500)
+          }
+        )
+    },
+    test (webview) {
+      const fs = require('fs')
+      const js = fs
+        .readFileSync('./src/renderer/components/Wechat/badge.js')
+        .toString()
+
+      webview.executeJavaScript(js)
     }
   }
 }
 </script>
 <style scoped>
+.indicator {
+  color: red;
+  font-size: 20px;
+}
 .wechat-page {
   width: 100%;
   height: 100%;
