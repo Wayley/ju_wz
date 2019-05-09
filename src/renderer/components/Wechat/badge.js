3159b77fc9723741bc0ef5869b815df5d4c91077
wzheng <hb_wangzheng@163.com>
2019-05-09 15:47:15 +0800

    modified badge.js

--- src/renderer/components/Wechat/badge.js
+++ src/renderer/components/Wechat/badge.js
@@ -1,22 +1,6 @@
-// // 引入 IPC 模块
-// const ipcRenderer = require('electron').ipcRenderer
+var webview = document.querySelector('webview')
+var nickNameEl = webview.querySelector('.header .info .nickname span')
+console.log(webview, 999, nickNameEl)
 
-// let badge = {
-//   get: function () {
-//     // 监听微信左侧面板节点变化
-//     $('.panel').bind('DOMSubtreeModified', function () {
-//       var count = 0
-//       // 累加所有未读消息
-//       $('.icon.web_wechat_reddot_middle').each(function () {
-//         count += parseInt(this.textContent)
-//       })
-//       // 通过 IPC 发送给主进程
-//       if (count > 0) {
-//         ipcRenderer.send('badge-changed', count.toString())
-//       } else {
-//         ipcRenderer.send('badge-changed', '')
-//       }
-//     })
-//   }
-// }
-// export default badge
+alert('test')
+alert(nickNameEl)
