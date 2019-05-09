19d2aa314585f756d64350e3fa75d7009692bfd6
wzheng <hb_wangzheng@163.com>
2019-05-09 16:37:18 +0800

    modified wechat component

--- src/renderer/components/Wechat/Wechat.vue
+++ src/renderer/components/Wechat/Wechat.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="wechat-page">
-    <div class="indicator"></div>
-    <webview id="foo" src="https://wx.qq.com/" style="display:inline-flex; width:900px; height:500px" nodeintegration allowpopups></webview>
+    <iframe id="frame-0" src="https://wx.qq.com" width="100%" height="100%" frameborder="0" scrolling="no" sandbox="allow-forms allow-modals allow-scripts allow-same-origin allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox"></iframe>
   </div>
 </template>
 <script>
@@ -11,95 +10,14 @@ export default {
   },
   created () {
     onload = () => {
-      const webview = document.querySelector('webview')
-      const indicator = document.querySelector('.indicator')
-      const loadstart = () => {
-        indicator.innerText = 'loading...'
-      }
-      const loadstop = () => {
-        indicator.innerText = 'loaded~~~~~'
-      }
-      webview.addEventListener('did-start-loading', loadstart)
-      webview.addEventListener('did-stop-loading', loadstop)
-
-      webview.addEventListener('new-window', e => {
-        console.log('new-window', e)
-      })
+      console.log(111, 'onload')
     }
   },
-  mounted () {
-    const webview = document.querySelector('webview')
-    webview.addEventListener('dom-ready', () => {
-      let result = webview.querySelector('div')
-      console.log('dom-ready', result)
-
-      this.insertCSS(webview)
-      this.executeJavaScript(webview)
-      // this.test(webview)
-    })
-    webview.addEventListener('did-finish-load', () => {})
-  },
-  methods: {
-    onload () {
-      console.log(11111)
-    },
-    insertCSS (webview) {
-      webview && webview.insertCSS(`body, p{color: red !important;}`)
-    },
-    executeJavaScript (webview) {
-      webview &&
-        webview.executeJavaScript(
-          `document.documentElement.innerHTML`,
-          // `document.querySelector('webview').documentElement.innerHTML`,
-          function (str) {
-            console.log(str)
-
-            let doc = document.createDocumentFragment()
-            let el = document.createElement('html')
-            el.innerHTML = str
-            doc.appendChild(el)
-            // ////////////////////
-            setTimeout(function () {
-              var userInfoEl = doc.querySelector('.header')
-              var imgEl = doc.querySelector('.header .img')
-              var nickNameEl = doc.querySelector('.header .info .nickname span')
-              var avatar = imgEl.src
-              var nickNameStr = nickNameEl.innerHTML
-
-              var appElement = doc.querySelector(
-                '[ng-controller=appController]'
-              )
-              const angular = window.angular
-
-              // 获取$scope变量
-              // var appElement =angular.element(document.findElementById('elementId');
-
-              var $scope = angular.element(appElement).scope()
-              console.log(appElement, '------------', $scope)
-
-              console.log(userInfoEl, 1234567890987654321, avatar, nickNameStr)
-              console.log('avatar:', avatar)
-              console.log('nickName:', nickNameStr)
-            }, 500)
-          }
-        )
-    },
-    test (webview) {
-      const fs = require('fs')
-      const js = fs
-        .readFileSync('./src/renderer/components/Wechat/badge.js')
-        .toString()
-
-      webview.executeJavaScript(js)
-    }
-  }
+  mounted () {},
+  methods: {}
 }
 </script>
 <style scoped>
-.indicator {
-  color: red;
-  font-size: 20px;
-}
 .wechat-page {
   width: 100%;
   height: 100%;
